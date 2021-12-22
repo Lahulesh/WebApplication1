733ebf4ef9eaff1e8c7f64dcde00f75fe6430bf5
Lahulesh Chourasiya <lahuleshchourasiya2000@gmail.com>
2021-12-22 12:05:57 +0530

    New ChangesAdd project files.

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication1.Controllers
+{
+    [EnableCors(origins: "https://localhost:44305/Home/Index",headers:"*",methods:"*")]
+    public class TestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Get: Test message")
+            };
+        }
+        public HttpResponseMessage Post()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Post: Test message")
+            };
+        }
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Put: Test message")
+            };
+        }
+    }
+}
