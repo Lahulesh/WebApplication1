97de3f50335767b7fad2c299b413517ac9747f46
Lahulesh Chourasiya <lahuleshchourasiya2000@gmail.com>
2021-12-22 12:05:57 +0530

    Add project files.

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Get: Test message")
+            };
+        }
+        public HttpResponseMessage Post()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Post: Test message")
+            };
+        }
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Put: Test message")
+            };
+        }
+    }
+}
